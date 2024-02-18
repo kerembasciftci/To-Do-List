9eab28306f3a86cc2824608ef7ffdaea25365c18
Kerem Başçiftçi <kerem.bascifci@gazi.edu.tr>
2024-02-18 12:59:15 +0300

    validation added -tedium

---
+++ Business/Validations/FluentValidation/MissionValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validations.FluentValidation
+{
+    public class MissionValidator : AbstractValidator<Mission>
+    {
+        public MissionValidator()
+        {
+            RuleFor(x=>x.Name).NotNull().WithMessage("Name can not be empty !").NotEmpty().WithMessage("Name is required !");
+            RuleFor(x => x.IsCompleted).NotNull();
+        }
+    }
+}
