1129f2a9b7d1ba5d666afc4596d521c5775a0e33
Kerem Başçiftçi <kerem.bascifci@gazi.edu.tr>
2024-02-20 18:33:54 +0300

    logging is completed -tedium

--- Core/Services/Service.cs
+++ Core/Services/Service.cs
@@ -1,6 +1,8 @@
 ﻿using Core.DataAccess.Repositories;
+using Core.Log;
 using Core.UnitOfWorks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,59 +16,140 @@ namespace Core.Services
     {
         private readonly IGenericRepository<T> _repository;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ILoggerService _logger;
 
-        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork, ILoggerService logger)
         {
             _repository = repository;
             _unitOfWork = unitOfWork;
+            _logger = logger;
         }
 
         public async Task<T> AddAsync(T entity)
         {
-            await _repository.AddAsync(entity);
-            await _unitOfWork.CommitAsync();
-            return entity;
+            try
+            {
+                await _repository.AddAsync(entity);
+                await _unitOfWork.CommitAsync();
+                _logger.LogInfo($"{typeof(T).Name} added successfully");
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error adding {typeof(T).Name}: {ex.Message}");
+                throw;
+
+            }
         }
 
         public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
         {
-            await _repository.AddRangeAsync(entities);
-            await _unitOfWork.CommitAsync();
-            return entities;
+            try
+            {
+                await _repository.AddRangeAsync(entities);
+                await _unitOfWork.CommitAsync();
+                _logger.LogInfo($"{typeof(T).Name} added successfully");
+                return entities;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error adding {typeof(T).Name}: {ex.Message}");
+                throw;
+            }
         }
 
-    
+
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await _repository.GetAll().ToListAsync();
+            try
+            {
+                var items = await _repository.GetAll().ToListAsync();
+                _logger.LogInfo($"{items.Count} items taken");
+                return items;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"GetAll method didnt work : {ex.Message}");
+                throw;
+            }
         }
 
         public async Task<T> GetByIdAsync(int id)
         {
-            return await _repository.GetByIdAsync(id);
+            try
+            {
+                var item = await _repository.GetByIdAsync(id);
+                _logger.LogInfo($"Id {id} item successfully came");
+                return item;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Id {id} item didnt came:{ex.Message}");
+                throw;
+            }
+
         }
 
         public async Task RemoveAsync(T entity)
         {
-            _repository.Remove(entity);
-            await _unitOfWork.CommitAsync();
+            try
+            {
+                _repository.Remove(entity);
+                await _unitOfWork.CommitAsync();
+                _logger.LogInfo($"{entity.GetType().Name} removed");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Entity didnt remove : {ex.Message}");
+                throw;
+            }
+           
         }
 
         public async Task RemoveRangeAsync(IEnumerable<T> entities)
         {
-            _repository.RemoveRange(entities);
-            await _unitOfWork.CommitAsync();
+            try
+            {
+                _repository.RemoveRange(entities);
+                await _unitOfWork.CommitAsync();
+                _logger.LogInfo("Successfuly entities removed !");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Entity didnt remove : {ex.Message}");
+                throw;
+            }
+          
         }
 
         public async Task UpdateAsync(T entity)
         {
-            _repository.Update(entity);
-            await _unitOfWork.CommitAsync();
+            try
+            {
+                _repository.Update(entity);
+                await _unitOfWork.CommitAsync();
+                _logger.LogInfo($"Updated {entity}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Entity didnt remove : {ex.Message}");
+                throw;
+            }
         }
 
         public IQueryable<T> Where(Expression<Func<T, bool>> expression)
         {
-            return _repository.Where(expression);
+            try
+            {
+                var item = _repository.Where(expression);
+                _logger.LogInfo("Element finded");
+                return item;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Element can not found :{ex.Message}");
+                throw;
+            }
         }
     }
 }
